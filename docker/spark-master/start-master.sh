c058c9c24b6404a48fde65bbe498a6d2cbbd1aab
Sébastien DA ROCHA <sebastien@da-rocha.net>
2019-07-11 17:14:10 +0200

    Faster docker restart

--- docker/spark-master/start-master.sh
+++ docker/spark-master/start-master.sh
@@ -12,4 +12,4 @@ export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
 
-cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
+cd /spark/bin && exec /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
